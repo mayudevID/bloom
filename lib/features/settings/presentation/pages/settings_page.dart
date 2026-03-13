feff26d033d5de0327eee15f99560322b236dfd6
Maulana Yusuf <66405774+mayudevID@users.noreply.github.com>
2026-03-13 09:25:55 +0700

    refactor: Remove commented-out backup functionality from SettingsPageContent

--- lib/features/settings/presentation/pages/settings_page.dart
+++ lib/features/settings/presentation/pages/settings_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:intl/intl.dart';
 
 import '../../../../core/routes/route_name.dart';
 import '../../../../core/utils/theme.dart';
@@ -84,123 +83,123 @@ class SettingsPageContent extends StatelessWidget {
                 ],
               ),
               const SizedBox(height: 51),
-              Row(
-                children: [
-                  Text(
-                    "Last backup:",
-                    style: textParagraph.copyWith(fontSize: 11),
-                  ),
-                  const SizedBox(
-                    width: 5,
-                  ),
-                  BlocBuilder<SettingsCubit, SettingsState>(
-                    builder: (context, state) {
-                      return Text(
-                        (state.updatedAt != DateTime(0))
-                            ? DateFormat('EEEE, dd MMMM y,')
-                                .add_jm()
-                                .format(state.updatedAt)
-                            : "No Updated",
-                        style: textParagraph.copyWith(fontSize: 11),
-                      );
-                    },
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              GestureDetector(
-                onTap: () {
-                  context.read<SettingsCubit>().backupData();
-                },
-                child: Container(
-                  height: 40,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: naturalWhite,
-                    border: Border.all(color: naturalBlack, width: 0.5),
-                  ),
-                  child: BlocConsumer<SettingsCubit, SettingsState>(
-                    listener: (context, state) {
-                      if (state.backupStatus == BackupStatus.success) {
-                        ScaffoldMessenger.of(context)
-                          ..hideCurrentSnackBar()
-                          ..showSnackBar(
-                            const SnackBar(
-                              content: Text("Backup success"),
-                            ),
-                          );
-                      } else if (state.backupStatus == BackupStatus.success) {
-                        ScaffoldMessenger.of(context)
-                          ..hideCurrentSnackBar()
-                          ..showSnackBar(
-                            const SnackBar(
-                              content: Text("Error: Unexpected"),
-                            ),
-                          );
-                      }
-                    },
-                    builder: (context, state) {
-                      if (state.backupStatus == BackupStatus.initial) {
-                        return Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const Icon(Icons.cached),
-                            const SizedBox(
-                              width: 5,
-                            ),
-                            Text(
-                              "Backup",
-                              style: buttonSmall,
-                            ),
-                          ],
-                        );
-                      } else if (state.backupStatus ==
-                          BackupStatus.processing) {
-                        return Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            SizedBox(
-                              width: 22,
-                              height: 22,
-                              child: CircularProgressIndicator(
-                                color: naturalBlack,
-                              ),
-                            ),
-                            const SizedBox(
-                              width: 15,
-                            ),
-                            Text(
-                              "Loading",
-                              style: buttonSmall.copyWith(
-                                color: naturalBlack,
-                              ),
-                            ),
-                          ],
-                        );
-                      } else {
-                        return Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const Icon(Icons.cached),
-                            const SizedBox(
-                              width: 5,
-                            ),
-                            Text(
-                              "Backup",
-                              style: buttonSmall,
-                            ),
-                          ],
-                        );
-                      }
-                    },
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 15,
-              ),
+              // Row(
+              //   children: [
+              //     Text(
+              //       "Last backup:",
+              //       style: textParagraph.copyWith(fontSize: 11),
+              //     ),
+              //     const SizedBox(
+              //       width: 5,
+              //     ),
+              //     BlocBuilder<SettingsCubit, SettingsState>(
+              //       builder: (context, state) {
+              //         return Text(
+              //           (state.updatedAt != DateTime(0))
+              //               ? DateFormat('EEEE, dd MMMM y,')
+              //                   .add_jm()
+              //                   .format(state.updatedAt)
+              //               : "No Updated",
+              //           style: textParagraph.copyWith(fontSize: 11),
+              //         );
+              //       },
+              //     ),
+              //   ],
+              // ),
+              // const SizedBox(
+              //   height: 10,
+              // ),
+              // GestureDetector(
+              //   onTap: () {
+              //     context.read<SettingsCubit>().backupData();
+              //   },
+              //   child: Container(
+              //     height: 40,
+              //     decoration: BoxDecoration(
+              //       borderRadius: BorderRadius.circular(10),
+              //       color: naturalWhite,
+              //       border: Border.all(color: naturalBlack, width: 0.5),
+              //     ),
+              //     child: BlocConsumer<SettingsCubit, SettingsState>(
+              //       listener: (context, state) {
+              //         if (state.backupStatus == BackupStatus.success) {
+              //           ScaffoldMessenger.of(context)
+              //             ..hideCurrentSnackBar()
+              //             ..showSnackBar(
+              //               const SnackBar(
+              //                 content: Text("Backup success"),
+              //               ),
+              //             );
+              //         } else if (state.backupStatus == BackupStatus.success) {
+              //           ScaffoldMessenger.of(context)
+              //             ..hideCurrentSnackBar()
+              //             ..showSnackBar(
+              //               const SnackBar(
+              //                 content: Text("Error: Unexpected"),
+              //               ),
+              //             );
+              //         }
+              //       },
+              //       builder: (context, state) {
+              //         if (state.backupStatus == BackupStatus.initial) {
+              //           return Row(
+              //             mainAxisAlignment: MainAxisAlignment.center,
+              //             children: [
+              //               const Icon(Icons.cached),
+              //               const SizedBox(
+              //                 width: 5,
+              //               ),
+              //               Text(
+              //                 "Backup",
+              //                 style: buttonSmall,
+              //               ),
+              //             ],
+              //           );
+              //         } else if (state.backupStatus ==
+              //             BackupStatus.processing) {
+              //           return Row(
+              //             mainAxisAlignment: MainAxisAlignment.center,
+              //             children: [
+              //               SizedBox(
+              //                 width: 22,
+              //                 height: 22,
+              //                 child: CircularProgressIndicator(
+              //                   color: naturalBlack,
+              //                 ),
+              //               ),
+              //               const SizedBox(
+              //                 width: 15,
+              //               ),
+              //               Text(
+              //                 "Loading",
+              //                 style: buttonSmall.copyWith(
+              //                   color: naturalBlack,
+              //                 ),
+              //               ),
+              //             ],
+              //           );
+              //         } else {
+              //           return Row(
+              //             mainAxisAlignment: MainAxisAlignment.center,
+              //             children: [
+              //               const Icon(Icons.cached),
+              //               const SizedBox(
+              //                 width: 5,
+              //               ),
+              //               Text(
+              //                 "Backup",
+              //                 style: buttonSmall,
+              //               ),
+              //             ],
+              //           );
+              //         }
+              //       },
+              //     ),
+              //   ),
+              // ),
+              // const SizedBox(
+              //   height: 15,
+              // ),
               GestureDetector(
                 onTap: () {
                   context.read<SettingsCubit>().logOut();
